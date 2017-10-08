fbb966bfe0976ac90d702aa65dcbdd28ee06dcf6
nadya-bu <bugakova_nadya@mail.ru>
2017-10-08 16:02:51 +0300

    Add crowler

--- Crawler.py
+++ Crawler.py
@@ -1,6 +1,16 @@
 class Crawler:
     def __init__(self, frontier):
         self.frontier = frontier
+        self.files = []
 
     def run(self):
-        pass
+        while not self.frontier.done():
+            website = self.frontier.next_site()
+            current_url = website.next_url()
+            if website.permits_craul(current_url):
+                if current_url.allow_store():
+                    file = current_url.store()
+                    self.files.append(file)
+                for url in current_url.extract_urls():
+                    self.frontier.add_url(url)
+            self.frontier.releaseSite(website)
