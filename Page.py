cdd33cddbe3c8845ccdbffab11909bb505fce822
LizaTretyakova <elizabet.tretyakova@gmail.com>
2017-10-08 16:26:50 +0300

    Page retrieval and url extraction

--- Page.py
+++ Page.py
@@ -1,9 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
 class Page:
     def __init__(self, url):
         self.url = url
 
     def retrieve(self):
-        pass
+        self._page = requests.get(self.url)
 
     def extract_urls(self):
-        pass
+        result = []
+        soup = BeautifulSoup(self._page, 'html.parser')
+        # TODO: check if I can follow links
+        for link in soup.find_all('a'):
+            result.append(link.get('href'))
+        return result
+
+    def allow_cache(self):
+        return True
+
+    def allow_index(self):
+        return True
+
+    def allow_follow(self):
+        return True
