618354c32da6eb3b8b8327bf8d775a9fa9924d13
LizaTretyakova <elizabet.tretyakova@gmail.com>
2017-10-08 17:37:57 +0300

    added site, checked permissions

--- Page.py
+++ Page.py
@@ -4,23 +4,31 @@ from bs4 import BeautifulSoup
 class Page:
     def __init__(self, url):
         self.url = url
+        self.soup = None
 
     def retrieve(self):
         self._page = requests.get(self.url)
 
     def extract_urls(self):
         result = []
-        soup = BeautifulSoup(self._page, 'html.parser')
-        # TODO: check if I can follow links
-        for link in soup.find_all('a'):
+        self.soup = BeautifulSoup(self._page, 'html.parser')
+        if not self.allow_follow():
+            return result
+        for link in self.soup.find_all('a'):
             result.append(link.get('href'))
         return result
 
     def allow_cache(self):
-        return True
+        return self.check_permission('NOARCHIVE')
 
     def allow_index(self):
-        return True
+        return self.check_permission('NOINDEX')
 
     def allow_follow(self):
+        return self.check_permission('NOFOLLOW')
+
+    def check_permission(self, perm):
+        for tag in self.soup.find_all('meta', name='ROBOTS'):
+            if perm in tag['content'].split(', '):
+                return False
         return True
--- Site.py
+++ Site.py
@@ -1,9 +1,26 @@
+import urllib.robotparser as urobot
+
 class Site:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, hostname):
+        self.hostname = hostname
+        self.iter = 0
+        self.urls = {0: self.hostname}
+        self._rp = urobot.RobotFileParser()
+        self._rp.set_url(hostname + 'robots.txt')
+        self._rp.read()
+
+    def update_urls(self, url):
+        if url not in self.urls.values():
+            cnt = len(self.urls)
+            self.urls[cnt] = url
+            return True
+        return False
 
     def next_url(self):
-        pass
+        if self.iter < len(self.urls):
+            self.iter += 1
+            return self.urls[self.iter - 1]
+        return None
 
     def permit_crawl(self, url):
-        pass
+        return self._rp.can_fetch('bot', url)
